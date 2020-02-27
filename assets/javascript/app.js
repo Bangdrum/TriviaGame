8959dc5a6b48be75c49a082f237acd94ca22590c
Ryan Shepard <bangdrumshep@gmail.com>
2020-02-26 23:43:23 -0800

    due date push

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,38 +1,44 @@
+$("#done").hide();
+
 $("#start").on('click', function () {
 
     game.start();
+    // $("#done").show();
 })
+
+
 
 var questions = [{
     question: "What car manufacturer makes the WRX STI?",
-    answers: ["Honda", "Lexus", "Toyota", "Subaru", "Mitsubishi"],
-    correctAnswer: "Subaru"
+    answers: ["Honda\xa0\xa0", "Lexus\xa0\xa0", "Toyota\xa0\xa0", "Subaru\xa0\xa0", "Mitsubishi\xa0\xa0"],
+    correctAnswer: "Subaru\xa0\xa0"
 }, {
     question: "Which car did Carroll Shelby help design for the world famous 'Le mans' race that won in 1966?",
-    answers: ["Chevy Corvette", "Ford MKII GT40", "BMW M1", "Ferrari 330 P3", "Porsche 911s"],
-    correctAnswer: "Ford MKII GT40"
+    answers: ["Chevy Corvette\xa0\xa0", "Ford MKII GT40\xa0\xa0", "BMW M1\xa0\xa0", "Ferrari 330 P3\xa0\xa0", "Porsche 911s\xa0\xa0"],
+    correctAnswer: "Ford MKII GT40\xa0\xa0"
 }, {
     question: "What is WHP an acronym for?",
-    answers: ["Where's Harry Potter?", "wired hood pin", "wheel horsepower", "winner's happy place", "with high probability"],
-    correctAnswer: "Wheel horsepower"
+    answers: ["Where's Harry Potter?\xa0\xa0", "wired hood pin\xa0\xa0", "wheel horsepower\xa0\xa0", "winner's happy place\xa0\xa0", "with high probability\xa0\xa0"],
+    correctAnswer: "wheel horsepower\xa0\xa0"
 }, {
     question: "Which production car holds the lap record at the Nordschleife layout of the Nurburgring in Germany?",
-    answers: ["Porsche 911 GT2 RS", "Porsche 911 GT3 RS", "Lamborghini Huracan LP 640-4 Performante", "Lamborghini Aventador LP770-4 SVJ", "Porsche 918 Spyder"],
-    correctAnswer: "Lamborghini Aventador LP770-4 SVJ"
+    answers: ["Porsche 911 GT2 RS\xa0\xa0", "Porsche 911 GT3 RS\xa0\xa0", "Lamborghini Huracan LP 640-4 Performante\xa0\xa0", "Lamborghini Aventador LP770-4 SVJ\xa0\xa0", "Porsche 918 Spyder\xa0\xa0"],
+    correctAnswer: "Lamborghini Aventador LP770-4 SVJ\xa0\xa0"
 }, {
     question: "Which Corvette is a rear engine vehicle? ",
-    answers: ["C5", "C6", "C7", "C8", "C4"],
-    correctAnswer: "C8"
+    answers: ["C5\xa0\xa0", "C6\xa0\xa0", "C7\xa0\xa0", "C8\xa0\xa0", "C4\xa0\xa0"],
+    correctAnswer: "C8\xa0\xa0"
 }, {
     question: "What is the purpose of Calipers and Rotors? ",
-    answers: ["Helps accelerate", "stops the car", "timing of engine", "transfers power to wheels", "checking tires for wear"],
-    correctAnswer: "stops the car"
+    answers: ["Helps accelerate\xa0\xa0", "stops the car\xa0\xa0", "timing of engine\xa0\xa0", "transfers power to wheels\xa0\xa0", "checking tires for wear\xa0\xa0"],
+    correctAnswer: "stops the car\xa0\xa0"
 }];
+// console.log(questions);
 
 var game = {
     correct: 0,
     incorrect: 0,
-    counter: 30,
+    counter: 45,
     countdown: function () {
 
         game.counter--;
@@ -44,15 +50,23 @@ var game = {
     },
     start: function () {
         timer = setInterval(game.countdown, 1000);
-        $('#altcontainer').prepend('<h2>Time Left: <span id="counter">30</span> Seconds</h2>');
+        $('#altcontainer').prepend('<h2>Time Left: <span id="counter">45</span> Seconds</h2>');
         $('#start').remove();
         for (var i = 0; i < questions.length; i++) {
             $('#altcontainer').append('<h2>' + questions[i].question + '</h2');
+
             for (var j = 0; j < questions[i].answers.length; j++) {
                 $("#altcontainer").append("<input type='radio' name='question-" + i + "' value='" + questions[i].answers[j] + "'>" + questions[i].answers[j]);
             }
+            if (i < (questions.length - 1)) {
+                $("#altcontainer").append("<hr>")
+            }
         }
+
     },
+
+
+
     done: function () {
         $.each($("input[name='question-0']:checked"), function () {
             if ($(this).val() == questions[0].correctAnswer) {
@@ -108,4 +122,7 @@ var game = {
 
         $('#altcontainer').append("<h3>Unanswered: " + (questions.length - (this.incorrect + this.correct)) + "</h3>");
     }
+
+
+
 }
